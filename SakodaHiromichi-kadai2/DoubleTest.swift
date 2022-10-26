14e3d2a2ebb438182421c6ffbe062a8b610014a8
sasasan03 <sako.hiro62@gmail.com>
2022-10-27 00:17:06 +0900

    課題２の添削をよろしくお願いします

---
+++ SakodaHiromichi-kadai2/DoubleTest.swift
@@ -0,0 +1,20 @@
+//
+//  DoubleTest.swift
+//  SakodaHiromichi-kadai2
+//
+//  Created by sako0602 on 2022/10/25.
+//
+
+import SwiftUI
+
+struct DoubleTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DoubleTest_Previews: PreviewProvider {
+    static var previews: some View {
+        DoubleTest()
+    }
+}
